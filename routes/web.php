8759162010c1ca805bfd2d1af426133f421edeab
Leonard Ugbanawaji Ekenekiso <senenerst@gmail.com>
2021-06-25 08:48:54 +0100

    routing for main site and tenants added

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,25 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
 Route::get('{any}', function () {
     return view('swapi.index');
 })->where('any', '.*');
+// Marketing site routes
+//Route::domain(env('APP_URL'))->group( function () {
+//    // Marketing home page
+//    Route::get('/', function (){
+//        return 'main site loaded';
+//    });
+//});
+
+// Customer routes
+//Route::domain('{account}.'.env('APP_URL'))->namespace('Store')->group(function () {
+//    // Customer home page route
+//    // All routes in this group will receive $account as first parameter
+//    // Use route–model binding to have $account be an Account instance
+//    Route::get('/', function (){
+//        return 'tenant sites loaded';
+//    });
+//});
+
+
